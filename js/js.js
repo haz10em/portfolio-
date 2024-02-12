fb003b8a9efab8243ed3177527bb1d54fb7c843d
haz10em <133292426+haz10em@users.noreply.github.com>
2024-02-12 05:10:40 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,23 @@
+// function toggle(){
+//     var menu=document.getElementById("menu");
+//     var cachosencel=document.getElementById("chose");
+//     var putton_cancel=document.getElementById("cancell");
+//     if(menu.style.display==="none"&&chose.style.display!=="block"||putton_cancel.style.display==="none"){
+//         menu.style.display="block";
+//         chose.style.display="none";
+//         putton_cancel.style.display="block";
+//     }
+//     else{
+//         menu.style.display="none";
+//         chose.style.display="block";
+//         putton_cancel.style.display="none";
+//     }
+// }
+
+function open1() {
+    document.getElementById("mySidebar").style.display = "block";
+  }
+  
+  function close1() {
+    document.getElementById("mySidebar").style.display = "none";
+  }
